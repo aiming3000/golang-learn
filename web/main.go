284a5d18b25d0a14cbb7aab6e48e8d237b6c52e9
liuaiming <15193790182>
2024-08-28 16:15:31 +0800

    go-micro web整合gin框架  路由分装

--- web/main.go
+++ web/main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
-	"fmt"
 	"github.com/gin-gonic/gin"
-	controller "mall/web/controller/user"
+	"go-micro.dev/v4/web"
+	"log"
+	all_router "mall/web/router"
 	"net/http"
+	//etcd1
+	"github.com/go-micro/plugins/v4/registry/etcd"
+	"go-micro.dev/v4/registry"
 )
 
 const addr = ":9000"
@@ -16,11 +20,35 @@ func Index(c *gin.Context) {
 }
 
 func main() {
+	//r := gin.Default()
+	//r.Handle("GET", "/", Index)
+	//r.Handle("POST", "/test-req", controller.AdminLogin)
+	//if err := r.Run(addr); err != nil {
+	//	fmt.Println("err")
+	//}
+
 	r := gin.Default()
-	r.Handle("GET", "/", Index)
-	r.Handle("POST", "/test-req", controller.AdminLogin)
-	if err := r.Run(addr); err != nil {
-		fmt.Println("err")
+
+	//设置注册中心
+	registerReg := etcd.NewRegistry(
+		registry.Addrs("127.0.0.1:2379"),
+	)
+	//注册路由组
+	all_router.InitRouter(r)
+	service := web.NewService(
+		web.Name("web"),
+		web.Version("latest"),
+		web.Handler(r),
+		web.Address(":8081"),
+		web.Registry(registerReg),
+	)
+	if err := service.Init(); err != nil {
+		log.Fatal(err)
+	}
+
+	// run service
+	if err := service.Run(); err != nil {
+		log.Fatal(err)
 	}
 
 }
